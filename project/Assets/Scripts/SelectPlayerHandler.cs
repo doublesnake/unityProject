ef81c52bd3aa396d1c9433cfc86451cf44535374
guiguiregis <towedesenou@yahoo.fr>
2015-01-21 23:14:54 -0500

    creation des scene

---
+++ project/Assets/Scripts/SelectPlayerHandler.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SelectPlayerHandler : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void backBtn(){
+		Application.LoadLevel ("StartScene");
+	}
+
+	public void StartBtn(){
+
+	}
+}
