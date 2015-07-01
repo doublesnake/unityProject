63086abfd342558b58e815a6b6b5618d485dfb00
Kevin <doublesnake78@hotmail.com>
2015-07-01 18:50:11 -0400

    ajout du dash

--- project/Assets/Character/Player.cs
+++ project/Assets/Character/Player.cs
@@ -5,18 +5,21 @@ public class Player : MonoBehaviour {
 
 	private Animator anim;
 	private CharacterController controller;
+	private InputControl input;
+	private Vector3 moveDirection = Vector3.zero;
 	public float speed = 4f;
 	public float gravity = 15.0f;
 	public float jumpPower = 5.0f;
+	public float dashPower = 20.0f;
+	// State variables
 	public bool facingRight = true;
 	public bool canJump = false;
-	public bool canHitAgain = true;
+	private bool enableMotion = true;
 	public bool isFalling = false;
-	public bool isIdle = true;
-	public Vector3 moveDirection = Vector3.zero;
 
 	// Use this for initialization
 	void Start () {
+		input = new InputControl ();
 		anim = gameObject.GetComponentInChildren<Animator>();
 		controller = gameObject.GetComponent<CharacterController> ();
 
@@ -25,10 +28,10 @@ public class Player : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		CheckStates ();
+		CheckInputs ();
 		SetAnimations ();
-		moveDirection.x = Input.GetAxisRaw("Horizontal");
-		controller.Move (moveDirection *speed* Time.deltaTime);
-		moveDirection.y -= gravity * Time.deltaTime;
+
+		controller.Move (moveDirection * speed * Time.deltaTime);
 		/*if (moveDirection.x > 0 && !facingRight)
 						Flip ();
 		else if(moveDirection.x < 0 && facingRight)
@@ -47,51 +50,121 @@ public class Player : MonoBehaviour {
 				else
 						isFalling = false;
 
+		enableMotion = true;
 	}
 
+	void CheckInputs()
+	{
+		
+		input.doubleTap = false;
+		if (Input.anyKeyDown) 
+		{
+			KeyCode currentKey = input.getCurrentKey();
+			if(input.tapCount == 1 && currentKey == input.lastKeyDown)
+			{
+
+				if(Time.time - input.lastTapTime < input.tapCooldown)
+				{
+					input.doubleTap = true;
+					input.tapCount = 0;
+				}
+			}
+			if(!input.doubleTap)
+			{
+				input.lastKeyDown = (currentKey!=KeyCode.None)? currentKey: input.lastKeyDown;
+				input.lastTapTime = Time.time;
+			
+			 	input.tapCount = 1;
+			}
+
+			
+			Debug.Log(currentKey.ToString());
+
+		}
+
+
+	}
 	void SetAnimations()
 	{
-		isIdle = true;
 		anim.SetBool ("walk_forward", false);
 		anim.SetBool ("walk_backward", false);
 		anim.SetBool ("idle",false);
 
-		
-		
-		if (Input.GetKey (KeyCode.D)) {
-			isIdle = false;
-			anim.SetTrigger("punch");
-				} else {
 
-						if (Input.GetKey ("right")) {
-								isIdle = false;
-								if (controller.isGrounded)
-										anim.SetBool ("walk_forward", true);
-						} else if (Input.GetKey ("left")) {
-								isIdle = false;
-								if (controller.isGrounded)
-										anim.SetBool ("walk_backward", true);
 
-						}
+		moveDirection.x = 0;
+
+		if(controller.isGrounded)
+		{
+			moveDirection.y = 0;
+			if (anim.GetCurrentAnimatorStateInfo(0).IsName("Punch"))
+			{
+				enableMotion = false;
+			}
+
+			if (input.isPunching()) {
+				enableMotion = false;
+				anim.CrossFade("Punch",0);
+			}
+			if (enableMotion) {
+				if (input.isMovingRight()) {
+						moveDirection.x = 1;
+					if(input.isDashing ())
+					{
+						moveDirection.x *= dashPower;
+					}
+					else
+						anim.SetBool ("walk_forward", true);
+				}
+				if (input.isMovingLeft()) {
+							moveDirection.x = -1;
+					if(input.isDashing ())
+					{
+						moveDirection.x *= dashPower;
+					}
+					anim.SetBool ("walk_backward", true);
+					}
 
 			
-						if (Input.GetKey ("up") && canJump) {
-								isIdle = false;
-								//anim.SetInteger ("AnimPar", 2);
-								moveDirection.y = Input.GetAxisRaw ("Vertical") * jumpPower;
-						} else if (isFalling) {
-								//anim.SetInteger ("AnimPar", 3);
-						}
+				if (input.isJumping() && canJump) {
+							moveDirection.y = jumpPower;
+					}
+			}
+		}
 
-				}
-		
-		if (isIdle) 
+		if(!controller.isGrounded)
 		{
-			if(controller.isGrounded)
-				anim.SetBool ("idle",true);
+			if (anim.GetCurrentAnimatorStateInfo(0).IsName("Punch"))
+			{
+				enableMotion = false;
+			}
+			
+			if (input.isPunching()) {
+				enableMotion = false;
+				//anim.SetTrigger("punch");
+				anim.CrossFade("Punch",0);
+			}
+
+			if (input.isMovingRight()) {
+					moveDirection.x = 1;
+				if(input.isDashing ())
+					moveDirection.x *= dashPower;
+
+			}
+			if (input.isMovingLeft()) {
+				moveDirection.x = -1;
+				if(input.isDashing ())
+					moveDirection.x *= dashPower;
+			}
+			
+			
+			if (isFalling) {
+
+			}
+
+			if(!input.isDashing()) moveDirection.y -= gravity * Time.deltaTime;
 		}
 	}
-
 	void Flip()
 		        {
 		facingRight = !facingRight;
@@ -99,4 +172,85 @@ public class Player : MonoBehaviour {
 		theScale.z *= -1;
 			transform.localScale = theScale;
 		}
+
+	public class InputControl
+	{
+		
+		public float tapCooldown = 0.3f;
+		public int tapCount = 0;
+		public float lastTapTime = 0;
+		public bool doubleTap = false;
+		public KeyCode lastKeyDown = KeyCode.None;
+
+
+
+		public KeyCode left;
+		public KeyCode up;
+		public KeyCode right;
+		public KeyCode punch;
+
+		private bool canMove;
+		private bool isDashingLeft;
+		private bool isDashingRight;
+
+
+		public InputControl()
+		{
+			left = KeyCode.A;
+			up = KeyCode.W;
+			right = KeyCode.D;
+			punch = KeyCode.Keypad1;
+		}
+
+		public bool isMovingLeft()
+		{
+			if (Input.GetKey (left)) 
+			{
+				isDashingLeft = doubleTap;
+				return true;
+			}
+			return false;
+		}
+		public bool isJumping()
+		{
+			if (Input.GetKey (up)) 
+					return true;
+			return false;
+		}
+		public bool isMovingRight()
+		{
+			if (Input.GetKey (right)) 
+			{
+				isDashingRight = doubleTap;
+				return true;
+			}
+			return false;
+		}
+		public bool isDashing()
+		{
+			if (isDashingLeft || isDashingRight)
+				return true;
+			return false;
+		}
+
+		public bool isPunching()
+		{
+			if (Input.GetKeyDown (punch))
+				return true;
+			return false;
+		}
+
+		public KeyCode getCurrentKey()
+		{
+			if (Input.GetKeyDown (left))
+					return left;
+			if (Input.GetKeyDown (right))
+					return right;
+			if (Input.GetKeyDown (up))
+					return up;
+			if (Input.GetKeyDown (punch))
+					return punch;
+			return KeyCode.None;
+		}
+	}
 }
